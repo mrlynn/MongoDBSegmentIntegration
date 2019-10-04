d689e80953ff06624ecf3619852e7e3ea9782268
Michael Lynn <merlynn@gmail.com>
2019-10-03 21:20:05 -0400

    retry stitch export

--- scripts/stitch-cli-export.sh
+++ scripts/stitch-cli-export.sh
@@ -2,9 +2,9 @@
 source .env
 echo Logging into Stitch
 stitch-cli --version
-#stitch-cli login --username=$STITCH_USERNAME --api-key=$STITCH_API_KEY
+stitch-cli login --username=$STITCH_USERNAME --api-key=$STITCH_API_KEY
 echo Exporting from Stitch 
-stitch-cli export --app-id=$STITCH_APPID --output=. --as-template --include-hosting
+stitch-cli export --app-id=$STITCH_APPID --output=./stitch_app --as-template --include-hosting
 echo Logging out
 #stitch-cli logout
 echo export complete
