5b72ea34babf792c1456ec3b3bc94807b5756c80
Michael Lynn <merlynn@gmail.com>
2019-10-11 11:37:25 -0400

    Update Stitch App

--- services/segmentHTTP/incoming_webhooks/webhook0/source.js
+++ services/segmentHTTP/incoming_webhooks/webhook0/source.js
@@ -1,4 +1,4 @@
-exports =  function(payload) {
+exports = function(payload) {
 
   var queryArg = payload.query.arg || '';
   var body = {};
@@ -10,7 +10,7 @@ exports =  function(payload) {
   // Get x-signature header and create digest for comparison
   const signature = payload.headers['X-Signature'];
   const digest = utils.crypto.hmac(payload.body.text(), context.values.get("segment_shared_secret"), "sha1", "hex");
-
+  
   //Only write the data if the digest matches Segment's x-signature!
   if (digest == signature) {
     
